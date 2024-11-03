dab14f22e5d8d416d7404441b37d44b13d5aee7c
shimomo <developer@shimomo.net>
2024-11-03 14:43:37 +0900

    Add http header to HttpBrowser

--- src/BaseCrawler.php
+++ src/BaseCrawler.php
@@ -27,7 +27,15 @@ abstract class BaseCrawler
      * @param  \Symfony\Component\BrowserKit\HttpBrowser  $httpBrowser
      * @return void
      */
-    public function __construct(protected HttpBrowser $httpBrowser){}
+    public function __construct(protected HttpBrowser $httpBrowser)
+    {
+        $this->httpBrowser->setServerParameters([
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'HTTP_ACCEPT_LANGUAGE' => 'ja;q=0.8',
+            'HTTP_CACHE_CONTROL' => 'max-age=0',
+        ]);
+    }
 
     /**
      * @param  \Symfony\Component\DomCrawler\Crawler  $crawler
